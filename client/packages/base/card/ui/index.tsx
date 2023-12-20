25bc862aa9548c15f51687fcf6cb345b036a5a07
sebbanwalker <seb10walker@hotmail.com>
2023-12-20 14:22:05 +0100

    card fixes

--- client/packages/base/card/ui/index.tsx
+++ client/packages/base/card/ui/index.tsx
@@ -1,27 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@zocom/button";
+
+export enum CardType {
+  "MENU" = "menu",
+  "SAUCE" = "sauce",
+  "CART" = "cart",
+}
 
 type CardProps = {
-    onClick?: () => void;
-    props: {
-        name: string,
-        price: number,
-        ingredients?: string[],
-        amount?: number,
-        sauces?: string[],
-    };
-}
+  onClick?: () => void;
+  state: CardType;
+  props: {
+    name?: string;
+    price?: number;
+    ingredients?: string[];
+    amount?: number;
+    dip?: {
+      name: string;
+      price: number;
+    }[];
+  };
+};
 
-export const Card = ({ props:{name, price, ingredients, amount, sauces}, onClick }: CardProps) => {
+export const Card = ({
+  props: { name, price, ingredients, amount, dip },
+  onClick,
+  state,
+}: CardProps) => {
   return (
     <article>
-        <section>
-            <h3>{name}</h3>
-            <aside>...........</aside>
-            <span>{price} SEK</span>
-        </section>
-        <p>{ingredients?.map((ingredient) => (
-            <>{ingredient},{" "}</>
-        ))}</p>
+      <section>
+        <h3>{name}</h3>
+        <aside>...........</aside>
+        <span>{price} SEK</span>
+      </section>
+      {state === CardType.MENU && <p>{ingredients?.join(", ")}</p>}
+      {state === CardType.SAUCE &&
+        dip &&
+        dip.map((sauce) => (
+          <Button key={sauce.name} onClick={() => console.log("hello")}>
+            {sauce.name}
+          </Button>
+        ))}
+      {state === CardType.CART && <></>}
+      <p>{state}</p>
     </article>
-  )
-}
+  );
+};
--- client/packages/pages/menupage/ui/index.tsx
+++ client/packages/pages/menupage/ui/index.tsx
@@ -1,13 +1,15 @@
-import './style.scss';
-import { wontons } from '../../../../../data/menu.json'
-import { Card } from '@zocom/card';
+import "./style.scss";
+import { wontons, dip } from "../../../../../data/menu.json";
+import { Card, CardType } from "@zocom/card";
 
 export const Menupage = () => {
   return (
     <section>
-      {wontons && wontons.map((wonton, index) => (
-        <Card props={wonton} key={index} />
-      ))}
+      {wontons &&
+        wontons.map((wonton, index) => (
+          <Card props={wonton} key={index} state={CardType.MENU} />
+        ))}
+      <Card props={dip} state={CardType.SAUCE} />
     </section>
-  )
-}
+  );
+};
