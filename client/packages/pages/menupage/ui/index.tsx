afe305eee923afad3ba1dad24d43167071698d1c
Joakim Sundman <joakimsundman89@gmail.com>
2023-12-20 10:08:01 +0100

    removed button

--- client/packages/pages/menupage/ui/index.tsx
+++ client/packages/pages/menupage/ui/index.tsx
@@ -1,12 +1,10 @@
 import './style.scss';
 
-import { CartButton } from '@zocom/cartbutton';
 
 export const Menupage = () => {
   return (
     <section>
       <h1>HEJ!</h1>
-      <CartButton />
     </section>
   )
 }
