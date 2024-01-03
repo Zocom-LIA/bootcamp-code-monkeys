27ee87d78df88ba56e7272865fcd7ea0768d667f
sebbanwalker <seb10walker@hotmail.com>
2024-01-03 14:21:53 +0100

    order page structure

---
+++ client/packages/pages/myorderpage/ui/index.tsx
@@ -0,0 +1,16 @@
+import "./style.scss";
+import { Header } from "@zocom/header";
+import React from "react";
+
+export const Myorderpage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <section className="menu">
+          <h2 className="menu__title">Cart</h2>
+        </section>
+      </main>
+    </>
+  );
+};
