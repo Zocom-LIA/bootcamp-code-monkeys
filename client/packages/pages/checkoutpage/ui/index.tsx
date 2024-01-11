1a5979c22d20072c965a78df126d1fc6c7375bfd
sebbanwalker <seb10walker@hotmail.com>
2024-01-11 13:26:08 +0100

    fixed refetch + styling

--- client/packages/pages/checkoutpage/ui/index.tsx
+++ client/packages/pages/checkoutpage/ui/index.tsx
@@ -6,14 +6,15 @@ import { Header } from "@zocom/header";
 import { Button, ButtonType } from "@zocom/button";
 import { StyleTypes, Order } from "@zocom/types";
 import { getOrder } from "@zocom/checkoutpage";
-import { CountdownTimer } from './feature/index';
+import { CountdownTimer } from "./feature/index";
 
 export const Checkoutpage = () => {
   const navigate = useNavigate();
-  
+
   const orderQuery = useQuery({
     queryKey: ["order"],
-    queryFn: getOrder
+    queryFn: getOrder,
+    refetchInterval: 1000 * 10,
   });
   orderQuery.refetch();
   const orderItem = orderQuery?.data?.order || [];
@@ -22,25 +23,40 @@ export const Checkoutpage = () => {
   if (orderQuery.isError) return <pre>{JSON.stringify(orderQuery.error)}</pre>;
 
   const isOrderType = (object: Order | never[]): object is Order =>
-  "id" in object || "orderStatus" in object;
+    "id" in object || "orderStatus" in object;
 
   const orderId = isOrderType(orderItem) ? orderItem.id : null;
-  const statusElement = isOrderType(orderItem) && orderItem.orderStatus === 'onGoing' ? 'Tillagas!' : 'Är klara!'  
+  const statusElement =
+    isOrderType(orderItem) && orderItem.orderStatus === "onGoing"
+      ? "Tillagas!"
+      : "Är klara!";
   const done = statusElement === "Är klara!";
-  
+
   return (
     <>
       <Header showLogo={true} showCart={false} />
       <main className={"checkout " + (done ? "done" : "")}>
         <section className="checkout__top">
-            <figure className="checkout__top--image"></figure>
-            <h2>Dina wontons {statusElement}</h2>
-            {!done ? <CountdownTimer /> : null}
-            <p>#{orderId}</p>
+          <figure className="checkout__top--image"></figure>
+          <h2>Dina wontons {statusElement}</h2>
+          {!done ? <CountdownTimer /> : null}
+          <p>#{orderId}</p>
         </section>
         <section className="checkout__bottom">
-        <Button type={ButtonType.CART} style={StyleTypes.DARK} onClick={() => navigate('/')}>Beställ mer</Button>
-        <Button type={ButtonType.CART} style={StyleTypes.BORDER} onClick={() => navigate('/receipt')}>Se kvitto</Button>
+          <Button
+            type={ButtonType.CART}
+            style={StyleTypes.DARK}
+            onClick={() => navigate("/")}
+          >
+            Beställ mer
+          </Button>
+          <Button
+            type={ButtonType.CART}
+            style={StyleTypes.BORDER}
+            onClick={() => navigate("/receipt")}
+          >
+            Se kvitto
+          </Button>
         </section>
       </main>
     </>
